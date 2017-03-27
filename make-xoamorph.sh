e9a90e46d65ccbccff70d50818971638fea55eca
Earl Ducaine <earl.ducaine-gmail-com>
2017-03-26 17:57:45 -0700

    Refining build script

--- make-xoamorph.sh
+++ make-xoamorph.sh
@@ -1,30 +1,32 @@
-
-
-
-
-
-
-
+#!/usr/bin/env bash
 
 function build_wx {
-    cd ~/dev/xara/wxGTK-2.8.12
-
+    if [ ! -d wxGTK-2.8.12 ]; then
+	if [ ! -f wxGTK-2.8.12.tar.gz ]; then
+	    curl -LO https://github.com/wxWidgets/wxWidgets/releases/download/v2.8.12/wxGTK-2.8.12.tar.gz
+	fi
+	tar xf wxGTK-2.8.12.tar.gz
+    fi
+    cd wxGTK-2.8.12
     if [ -d buildgtk ]; then
 	cd buildgtk
 	sudo make uninstall
 	cd ..
 	rm -rf buildgtk
-	mkdir buildgtk
     fi
+    mkdir buildgtk
     cd buildgtk
     ../configure --with-gtk --enable-unicode
     make -j 8
     sudo make install
     sudo ldconfig
+    cd ..
+    cd ..
 }
 
-
-cd xoamorph
-./autogen.sh
-./configure
-make
+function build_xoamorph {
+    cd xoamorph
+    ./autogen.sh
+    ./configure
+    make
+}
