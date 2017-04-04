e61246bd439a569a8c99179e6795e0e68125c865
Earl Ducaine <earl.ducaine-gmail-com>
2017-04-03 22:52:21 -0700

    Eliminate compiler warnings

--- wxXtra/gridcombo.cpp
+++ wxXtra/gridcombo.cpp
@@ -42,7 +42,7 @@ Function  : wxGridComboPopup::GetColumns
 Author    : Mikhail Tatarnikov
 Purpose   : A helper function to obtain the number of columns specified by the user
 Returns   : int - the number of columns to display in the popup.
-Exceptions: 
+Exceptions:
 Parameters: None
 Notes     : All the information is stored in one place - the combobox control itself.
 ******************************************************************************/
@@ -57,7 +57,7 @@ Author    : Mikhail Tatarnikov
 Purpose   : A helper function to obtain the number of rows that should be
 			displayd. Not all rows will be visible at once.
 Returns   : int - the number of all rows (visible and invisible).
-Exceptions: 
+Exceptions:
 Parameters: None
 Notes     : All the required to calculate the rows coun is stored in one
 			place - the combobox control itself.
@@ -72,7 +72,7 @@ Function  : intwxGridComboPopup::GetItemsNum
 Author    : Mikhail Tatarnikov
 Purpose   : A helper function to obtain the number of items in the combobx.
 Returns   : int - the number of items to display.
-Exceptions: 
+Exceptions:
 Parameters: None
 Notes     : All the information is stored in one place - the combobox control itself.
 ******************************************************************************/
@@ -87,7 +87,7 @@ Author    : Mikhail Tatarnikov
 Purpose   : A helper function to obtain the size of items - e.g. the popup cell
 			size.
 Returns   : wxSize - the items size.
-Exceptions: 
+Exceptions:
 Parameters: None
 Notes     : All the information is stored in one place - the combobox control itself.
 			All items have the same size.
@@ -102,9 +102,9 @@ Function  : wxGridComboPopup::GetSelected
 Author    : Mikhail Tatarnikov
 Purpose   : Get the currentrly selected element.
 Returns   : int - the index of currently selected element, -1 if none.
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 int wxGridComboPopup::GetSelected() const
 {
@@ -117,9 +117,9 @@ Function  : wxGridComboPopup::SetSelected
 Author    : Mikhail Tatarnikov
 Purpose   : Selects an element
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iItem - an element index to select.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::SetSelected(int iItem)
 {
@@ -132,9 +132,9 @@ Function  : wxGridComboPopup::Init
 Author    : Mikhail Tatarnikov
 Purpose   : Performs any initialization required.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::Init()
 {
@@ -146,9 +146,9 @@ Function  : wxGridComboPopup::Create
 Author    : Mikhail Tatarnikov
 Purpose   : Creates the popup window
 Returns   : bool - true if successfull, false otherwise.
-Exceptions: 
+Exceptions:
 Parameters: [in] wxWindow* pwndParent - the parent window (combobox control).
-Notes     : 
+Notes     :
 ******************************************************************************/
 bool wxGridComboPopup::Create(wxWindow* pwndParent)
 {
@@ -166,7 +166,7 @@ Function  : wxGridComboPopup::ChangeSelection
 Author    : Mikhail Tatarnikov
 Purpose   : Notification that the selection has been changed.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iOldSelected - the prevoiusly selected item;
             [in] int iNewSelected - the newly selected item.
 Notes     : Control has to repqint both items.
@@ -183,9 +183,9 @@ Author    : Mikhail Tatarnikov
 Purpose   : Update the scroll bars with the current information
 			(items number, columns, sizes, etc).
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::UpdateScrollers()
 {
@@ -206,9 +206,9 @@ Function  : wxGridComboPopup::ItemToGrid
 Author    : Mikhail Tatarnikov
 Purpose   : Calculates position of an item based on its index
 Returns   : wxPoint - the item position (row/column).
-Exceptions: 
+Exceptions:
 Parameters: [in] int iItem - item index to calculate positio for.
-Notes     : 
+Notes     :
 ******************************************************************************/
 wxPoint wxGridComboPopup::ItemToGrid(int iItem)
 {
@@ -225,9 +225,9 @@ Function  : wxGridComboPopup::ItemFromPoint
 Author    : Mikhail Tatarnikov
 Purpose   : Finds item based on point (in client virtual coordinates)
 Returns   : int - the item index which the point belong.
-Exceptions: 
+Exceptions:
 Parameters: [in] wxPoint ptClient - the point to find item from.
-Notes     : 
+Notes     :
 ******************************************************************************/
 int wxGridComboPopup::ItemFromPoint(wxPoint ptClient)
 {
@@ -240,7 +240,7 @@ int wxGridComboPopup::ItemFromPoint(wxPoint ptClient)
 		wxRect rcItem = GetItemRect(i);
 
 		// Check if the item should be updated.
-		if (rcItem.Inside(ptClient))
+		if (rcItem.Contains(ptClient))
 			return i;
 	}
 
@@ -252,9 +252,9 @@ Function  : wxGridComboPopup::GetItemRect
 Author    : Mikhail Tatarnikov
 Purpose   : Calculates the item area (in client virtual coordinates)
 Returns   : wxRect - the area occupied by the item.
-Exceptions: 
+Exceptions:
 Parameters: [in] int iItem - item to calculate the area for.
-Notes     : 
+Notes     :
 ******************************************************************************/
 wxRect wxGridComboPopup::GetItemRect(int iItem)
 {
@@ -275,10 +275,10 @@ Function  : wxGridComboPopup::ClientToScrolledVirtual
 Author    : Mikhail Tatarnikov
 Purpose   : Transfers a point from client to virtual (scrolled) coordinates
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in/out] int* pX - the absciss value to convert;
             [in/out] int* pY - the ordinate value to convert.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::ClientToScrolledVirtual(int* pX, int* pY)
 {
@@ -286,10 +286,10 @@ void wxGridComboPopup::ClientToScrolledVirtual(int* pX, int* pY)
 	int iXOrigin = 0;
 	int iYOrigin = 0;
 	GetViewStart(&iXOrigin, &iYOrigin);
-	
+
 	if (pX)
 		*pX += iXOrigin * szItem.x;
-		
+
 	if (pY)
 		*pY += iYOrigin * szItem.y;
 
@@ -301,9 +301,9 @@ Function  : wxGridComboPopup::OnShow
 Author    : Mikhail Tatarnikov
 Purpose   : Notification that the popup is about to be shown
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::OnShow()
 {
@@ -314,14 +314,14 @@ Function  : wxGridComboPopup::OnSize
 Author    : Mikhail Tatarnikov
 Purpose   : Resize notification handler.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxSizeEvent& event - size event information.
 Notes     : We need to update our scroll bars.
 ******************************************************************************/
 void wxGridComboPopup::OnSize(wxSizeEvent& event)
 {
 	wxScrolledWindow::OnSize(event);
-	
+
 	UpdateScrollers();
 }
 
@@ -331,15 +331,15 @@ Function  : wxGridComboPopup::OnMouseMove
 Author    : Mikhail Tatarnikov
 Purpose   : mouse movement notification handler
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxMouseEvent& event - mouse event information.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::OnMouseMove(wxMouseEvent& event)
 {
 	// Convert the poin to the logical coordinates.
 	wxPoint ptMouse(event.GetX(), event.GetY());
-	ClientToScrolledVirtual(&ptMouse.x, &ptMouse.y);	
+	ClientToScrolledVirtual(&ptMouse.x, &ptMouse.y);
 
 	// Find out what item the mouse hover over.
 	int iItem = ItemFromPoint(ptMouse);
@@ -366,7 +366,7 @@ Function  : wxGridComboPopup::OnTimer
 Author    : Mikhail Tatarnikov
 Purpose   : Timer notification handler
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxTimerEvent& event - time event information.
 Notes     : We need it to trace situations when the mouse highlight an item and
 			then leave the control leaving the item highlighted.
@@ -389,7 +389,7 @@ void wxGridComboPopup::OnTimer(wxTimerEvent& event)
 	if (iItem == -1)
 	{
 		m_tmrHighlited.Stop();
-		
+
 		int iCurHighlighted = m_iHighlighed;
 		m_iHighlighed		 = -1;
 		InvalidateItem(iCurHighlighted);
@@ -403,9 +403,9 @@ Function  : wxGridComboPopup::OnKey
 Author    : Mikhail Tatarnikov
 Purpose   : Keyboard notification handler.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxKeyEvent& event - keyboard event information.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::OnKey(wxKeyEvent& event)
 {
@@ -417,16 +417,16 @@ Function  : wxGridComboPopup::OnLeftClick
 Author    : Mikhail Tatarnikov
 Purpose   : Left mouse button click handler
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxMouseEvent& event - mouse event information.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::OnLeftClick(wxMouseEvent& event)
 {
 	// Convert the poin to the logical coordinates.
 	wxPoint ptMouse(event.GetX(), event.GetY());
-	ClientToScrolledVirtual(&ptMouse.x, &ptMouse.y);	
-	
+	ClientToScrolledVirtual(&ptMouse.x, &ptMouse.y);
+
 	// Select a new item, if any was clicked.
 	int iItem = ItemFromPoint(ptMouse);
 	if (iItem != -1)
@@ -441,9 +441,9 @@ Author    : Mikhail Tatarnikov
 Purpose   : Helper  function to setup a new selection, close the popup and send
 			a notification message.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::DismissWithEvent()
 {
@@ -458,9 +458,9 @@ Function  : wxGridComboPopup::SendComboBoxEvent
 Author    : Mikhail Tatarnikov
 Purpose   : Sends a combobox selection event
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iSelection - a new selection to notify clients about.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::SendComboBoxEvent(int iSelection)
 {
@@ -486,12 +486,12 @@ Function  : wxGridComboPopup::GetAdjustedSize
 Author    : Mikhail Tatarnikov
 Purpose   : Calculates preferable size.
 Returns   : wxSize - the final size requested.
-Exceptions: 
+Exceptions:
 Parameters: [in] int iMinWidth	 - minimum width for the control. In some circumstances
 								   can be ignored;
             [in] int iPrefHeight - preferable height;
             [in] int iMaxHeight	 - maximum of height allowed.
-Notes     : 
+Notes     :
 ******************************************************************************/
 wxSize wxGridComboPopup::GetAdjustedSize(int iMinWidth, int iPrefHeight, int iMaxHeight)
 {
@@ -499,7 +499,7 @@ wxSize wxGridComboPopup::GetAdjustedSize(int iMinWidth, int iPrefHeight, int iMa
 	wxSize szItem	= GetItemSize();
 	int	   iColumns = GetColumns();
 	int	   iRows	= GetRows();
-	
+
 	szAdjusted.x = szItem.x * iColumns;
 
 	int iRowsFit = iPrefHeight / szItem.y + 1;
@@ -511,7 +511,7 @@ wxSize wxGridComboPopup::GetAdjustedSize(int iMinWidth, int iPrefHeight, int iMa
 
 	// We must show at least one row.
 	iRowsFit = std::max(iRowsFit, 1);
-	
+
 	// No point in showing more rows than we have.
 	iRowsFit = std::min(iRowsFit, iRows);
 
@@ -524,8 +524,8 @@ wxSize wxGridComboPopup::GetAdjustedSize(int iMinWidth, int iPrefHeight, int iMa
 	// Take into account the border.
 	szAdjusted.x += 2;
 	szAdjusted.y += 2;
-	
-	
+
+
 	return szAdjusted;
 }
 
@@ -534,9 +534,9 @@ Function  : wxGridComboPopup::InvalidateItem
 Author    : Mikhail Tatarnikov
 Purpose   : Requests repaint of an item.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iItem - an item to repaint.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::InvalidateItem(int iItem)
 {
@@ -545,13 +545,13 @@ void wxGridComboPopup::InvalidateItem(int iItem)
 
 	int iX = 0;
 	int iY = 0;
-	
+
 	wxRect rcItem = GetItemRect(iItem);
-	
+
 	ClientToScrolledVirtual(&iX, &iY);
 	rcItem.x -= iX;
 	rcItem.y -= iY;
-	
+
 	Refresh(false, &rcItem);
 }
 
@@ -561,7 +561,7 @@ Function  : wxGridComboPopup::OnDraw
 Author    : Mikhail Tatarnikov
 Purpose   : The default implementation of drawing.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxDC& dc - device context of the control to paint to.
 Notes     : Breaks the control to separate items and sends a request for painting
 			each item via DrawItem pure virtual function.
@@ -602,7 +602,7 @@ Function  : wxGridComboPopup::PaintComboControl
 Author    : Mikhail Tatarnikov
 Purpose   : Paints an item in the combo control itself.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxDC&		   dc	- dc to paint to;
             [in] const wxRect& rect - area for painting.
 Notes     : Uses DrawItem pure virtual function.
@@ -629,7 +629,7 @@ Function  : wxGridComboPopup::DrawItem
 Author    : Mikhail Tatarnikov
 Purpose   : Draws item based on row and column
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxDC&	 dc		   - device context to paint to;
             [in] wxRect& rcRect	   - the item position;
             [in] int	 iRow	   - element's row;
@@ -655,9 +655,9 @@ Function  : wxGridComboPopup::UpdateColumnsNum
 Author    : Mikhail Tatarnikov
 Purpose   : Updates the coumn number.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridComboPopup::UpdateColumnsNum()
 {
@@ -680,9 +680,9 @@ Function  : wxGridCombo::Init
 Author    : Mikhail Tatarnikov
 Purpose   : Initialization routine
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::Init()
 {
@@ -706,9 +706,9 @@ Function  : wxGridCombo::Create
 Author    : Mikhail Tatarnikov
 Purpose   : Creates the control
 Returns   : bool - see wxWindow::Create
-Exceptions: 
+Exceptions:
 Parameters: see wxWindow::Create.
-Notes     : 
+Notes     :
 ******************************************************************************/
 bool wxGridCombo::Create(wxWindow* pwndParent,
 						   wxWindowID wiID,
@@ -728,7 +728,7 @@ Function  : wxGridCombo::~wxGridCombo
 Author    : Mikhail Tatarnikov
 Purpose   : Destroys the object
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
 Notes     :
 ******************************************************************************/
@@ -742,9 +742,9 @@ Function  : wxGridCombo::DeleteUserData
 Author    : Mikhail Tatarnikov
 Purpose   : Deletes all user data for itmes.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::DeleteUserData()
 {
@@ -760,14 +760,14 @@ Function  : wxGridCombo::AddItem
 Author    : Mikhail Tatarnikov
 Purpose   : Adds a new item (appended to the end)
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] CGridComboUserData* pUserData - a new item to add.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::AddItem(CGridComboUserData* pUserData)
 {
 	m_vecUserData.push_back(pUserData);
-	
+
 	wxGridComboPopup* pPopup = (wxGridComboPopup*)GetPopupControl();
 	if (pPopup)
 		pPopup->UpdateScrollers();
@@ -778,14 +778,14 @@ Function  : wxGridCombo::Clear
 Author    : Mikhail Tatarnikov
 Purpose   : Removes all elements from the combobox
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::Clear()
 {
 	DeleteUserData();
-	
+
 	wxGridComboPopup* pPopup = (wxGridComboPopup*)GetPopupControl();
 
 	if (pPopup)
@@ -798,9 +798,9 @@ Function  : wxGridCombo::GetUserData
 Author    : Mikhail Tatarnikov
 Purpose   : Retrieves user data associated with an item.
 Returns   : CGridComboUserData* - the user data for the item, if any.
-Exceptions: 
+Exceptions:
 Parameters: [in] int iIndex - index of the item to retrieve associated user data for.
-Notes     : 
+Notes     :
 ******************************************************************************/
 CGridComboUserData* wxGridCombo::GetUserData(int iIndex)
 {
@@ -815,9 +815,9 @@ Function  : wxGridCombo::GetRows
 Author    : Mikhail Tatarnikov
 Purpose   : Calculates the number of rows.
 Returns   : int - the number of rows.
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 int wxGridCombo::GetRows()
 {
@@ -836,7 +836,7 @@ Function  : wxGridCombo::SetItemSize
 Author    : Mikhail Tatarnikov
 Purpose   : Sets the item size
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] wxSize szItem - a new size for items.
 Notes     : All items have the same size.
 ******************************************************************************/
@@ -858,14 +858,14 @@ Function  : wxGridCombo::SetColumns
 Author    : Mikhail Tatarnikov
 Purpose   : Sets the number of columns
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iCols - new column number.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::SetColumns(int iCols)
 {
 	m_iColumns = iCols;
-	
+
 	wxGridComboPopup* pPopup = (wxGridComboPopup*)GetPopupControl();
 
 	if (!pPopup)
@@ -880,9 +880,9 @@ Function  : wxGridCombo::SetSelected
 Author    : Mikhail Tatarnikov
 Purpose   : Selects an item.
 Returns   : void
-Exceptions: 
+Exceptions:
 Parameters: [in] int iItem - an item to select, -1 for unselecting.
-Notes     : 
+Notes     :
 ******************************************************************************/
 void wxGridCombo::SetSelected(int iItem)
 {
@@ -908,9 +908,9 @@ Function  : wxGridCombo::GetSelected
 Author    : Mikhail Tatarnikov
 Purpose   : Gets the selected item
 Returns   : int - the index of the selected item, -1 otherwise.
-Exceptions: 
+Exceptions:
 Parameters: None
-Notes     : 
+Notes     :
 ******************************************************************************/
 int wxGridCombo::GetSelected() const
 {
