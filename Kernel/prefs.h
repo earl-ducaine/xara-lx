24f6ff81bb5485b8989aecf4a28656bf18a0a15f
Earl Ducaine <earl.ducaine-gmail-com>
2017-04-07 19:40:13 -0700

    Fix problem with build

--- Kernel/prefs.h
+++ Kernel/prefs.h
@@ -166,16 +166,16 @@ public:
 
 	BOOL DeclareSection(TCHAR *Section, UINT32 InitialSize);
 
-	BOOL DeclarePref(TCHAR *Section, const TCHAR *Pref,
+	BOOL DeclarePref(TCHAR *Section, TCHAR *Pref,
 					 INT32 *PrefVar, INT32 Min = INT_MIN, INT32 Max = INT_MAX);
 
-	BOOL DeclarePref(TCHAR *Section, const TCHAR *Pref,
+	BOOL DeclarePref(TCHAR *Section, TCHAR *Pref,
 					 UINT32 *PrefVar, UINT32 Min = 0, UINT32 Max = UINT_MAX);
 
-	BOOL DeclarePref(TCHAR *Section, const TCHAR *Pref,
+	BOOL DeclarePref(TCHAR *Section, TCHAR *Pref,
 					 double *PrefVar, double Min = DBL_MIN, double Max = DBL_MAX);
 
-	BOOL DeclarePref(TCHAR *Section, const TCHAR *Pref, StringBase *PrefVar);
+	BOOL DeclarePref(TCHAR *Section, TCHAR *Pref, StringBase *PrefVar);
 
 	// These next functions allow the preferences to be read/written by other
 	// people than the owner e.g. the application options dialog box
