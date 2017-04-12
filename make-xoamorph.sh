d19e111f3f8d2126a34438a12f75609d4d8abbe8
Earl Ducaine <earl.ducaine-gmail-com>
2017-04-12 00:22:05 -0700

    Misc. build utilities

--- make-xoamorph.sh
+++ make-xoamorph.sh
@@ -25,26 +25,15 @@ function build_wx {
 }
 
 function build_png {
-    if [ ! -d wxGTK-2.8.12 ]; then
-	if [ ! -f wxGTK-2.8.12.tar.gz ]; then
-	    https://superb-sea2.dl.sourceforge.net/project/libpng/libpng12/1.2.57/libpng-1.2.57.tar.gz
-	    curl -LO https://github.com/wxWidgets/wxWidgets/releases/download/v2.8.12/wxGTK-2.8.12.tar.gz
+    if [ ! -d libpng-1.2.57 ]; then
+	if [ ! -f libpng-1.2.57.tar.gz ]; then
+	    curl -LO https://superb-sea2.dl.sourceforge.net/project/libpng/libpng12/1.2.57/libpng-1.2.57.tar.gz
 	fi
-	tar xf wxGTK-2.8.12.tar.gz
+	tar xf libpng-1.2.57.tar.gz
     fi
-    cd wxGTK-2.8.12
-    if [ -d buildgtk ]; then
-	cd buildgtk
-	sudo make uninstall
-	cd ..
-	rm -rf buildgtk
-    fi
-    mkdir buildgtk
-    cd buildgtk
-    ../configure --with-gtk --enable-unicode
-    make -j 8
-    sudo make install
-    sudo ldconfig
+    cd libpng-1.2.57
+    ./configure
+    make -j 2
     cd ..
     cd ..
 }
