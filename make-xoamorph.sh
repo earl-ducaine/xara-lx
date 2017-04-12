957617d681b5b01e947829719adbe93c3aba3f2a
Earl Ducaine <earl.ducaine-gmail-com>
2017-04-11 23:44:14 -0700

    Misc. build utilities

--- make-xoamorph.sh
+++ make-xoamorph.sh
@@ -27,6 +27,7 @@ function build_wx {
 function build_png {
     if [ ! -d wxGTK-2.8.12 ]; then
 	if [ ! -f wxGTK-2.8.12.tar.gz ]; then
+	    https://superb-sea2.dl.sourceforge.net/project/libpng/libpng12/1.2.57/libpng-1.2.57.tar.gz
 	    curl -LO https://github.com/wxWidgets/wxWidgets/releases/download/v2.8.12/wxGTK-2.8.12.tar.gz
 	fi
 	tar xf wxGTK-2.8.12.tar.gz
