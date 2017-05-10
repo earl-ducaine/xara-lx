aee75b06c040ba7abc56eda71da1a0737293d519
Earl Ducaine <earl.ducaine-gmail-com>
2017-05-10 03:16:29 -0700

    Embed assembly for GenerateWallShadow in function

--- libs/x86_64/orphaned-object-files/objcopy-example.sh
+++ libs/x86_64/orphaned-object-files/objcopy-example.sh
@@ -3,7 +3,7 @@
 
 
 objcopy \
-    -N _Z19GenerateFloorShadowPvPK16BITMAPINFOHEADERPKhS2_PhjPKiS7_jS4_ \
+    -N _Z18GenerateWallShadowPK16BITMAPINFOHEADERPKhS1_PhjPKjS6_jS6_S6_jS3_ \
     libCDraw_la-bshadow2.o \
     libCDraw_la-bshadow2-new.o
 	
