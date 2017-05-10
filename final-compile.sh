56cfc91f56d7a7f6ce16e0bb5c784e7c97817dc1
Earl Ducaine <earl.ducaine-gmail-com>
2017-05-10 04:04:56 -0700

    Embed assembly for GenerateWallShadow in function

---
+++ final-compile.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+g++ -v  -no-pie -Wl,--start-group Kernel/libKernel.a wxOil/libwxOil.a tools/libTools.a wxXtra/libwxXtra.a -Wl,--end-group --debug -L./libs/x86_64  -L/usr/local/lib -pthread   -lwx_gtk2u_richtext-2.8 -lwx_gtk2u_aui-2.8 -lwx_gtk2u_xrc-2.8 -lwx_gtk2u_qa-2.8 -lwx_gtk2u_html-2.8 -lwx_gtk2u_adv-2.8 -lwx_gtk2u_core-2.8 -lwx_baseu_xml-2.8 -lwx_baseu_net-2.8 -lwx_baseu-2.8   -lxml2 -ljpeg -lCDraw -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgio-2.0 -lpangoft2-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo -lpango-1.0 -lfreetype -lfontconfig -lgobject-2.0 -lglib-2.0 ./libpng-1.2.57/.libs/libpng.a -lz -o XaraLX
