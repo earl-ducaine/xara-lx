c87b058390240ba12e5617bd12317f5764b5646a
Earl Ducaine <earl.ducaine-gmail-com>
2017-05-25 00:04:38 -0700

    Build: hacking wxGetRootWindow to be unique file: wxOil/ftfonts.cpp Also misc build script tweaking.

--- make-xoamorph.sh
+++ make-xoamorph.sh
@@ -102,10 +102,13 @@ function build_xoamorph {
     # cd ../..
     # ar -xv libCDraw.a 
     # export PATH="/usr/lib/ccache:$PATH"; make -j 4 1> make-out.txt 2>make-error.txt
-    make -j 4 1> make-out.txt 2>make-error.txt
+    make -j 4
     # make -j 4
 }
 
+function last_step {
+    g++ -no-pie -Wl,--start-group Kernel/libKernel.a wxOil/libwxOil.a tools/libTools.a wxXtra/libwxXtra.a -Wl,--end-group --debug -L./libs/x86_64 -L./libs/x86_64/cdraw-alt  -L/usr/local/lib -pthread -lwx_gtk2u_aui-3.0 -lwx_gtk2u_xrc-3.0 -lwx_gtk2u_html-3.0 -lwx_gtk2u_qa-3.0 -lwx_gtk2u_adv-3.0 -lwx_gtk2u_core-3.0 -lwx_baseu_xml-3.0 -lwx_baseu_net-3.0 -lwx_baseu-3.0    -lxml2 -ljpeg -lCDraw -lCDraw-alt -lgtk-x11-2.0 -lgdk-x11-2.0 -latk-1.0 -lgio-2.0 -lpangoft2-1.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo -lpango-1.0 -lfreetype -lfontconfig -lgobject-2.0 -lglib-2.0 ./libpng-1.2.57/.libs/libpng.a -lz -o XaraLX
+}
 
 
 function make_tags {
