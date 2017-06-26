e37f2611a64dbbdec114e30aef67cd516f17c885
me <me>
2017-06-25 22:15:54 -0700

    Updates for troubleshooting fill

---
+++ gdb-command.cmd
@@ -0,0 +1,11 @@
+
+# printf "%x",   RenderPen[CurrentPen].GetColour().GetRGB()
+
+b OSRenderRegion::CreateNewPen
+
+
+b NodeRegularShape::RenderEorDrag
+
+# together
+b OpNewRegShape::RenderDragBlobs
+b RenderRegion::SetFillColour
